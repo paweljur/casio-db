3c57ddf81218c7e9cfd7e978c7d2536f638e1242
Paweł Jur <pawel.tomasz.jur@gmail.com>
2023-10-18 13:50:08 +0200

    feature: show message from db in view

--- casio_db/casio_db/views.py
+++ casio_db/casio_db/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+from .models import Message
 
 def hello_world(request):
-    return HttpResponse("Hello world!")
+    try:
+        message = Message.objects.first().content
+    except AttributeError:
+        message = "Theres nothing to display"
+    return HttpResponse(message)
